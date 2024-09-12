27aecd316563b6be7293217dc0460a4eed4e2c0b
Poonam-gaian <poonam.r@gaiansolutions.com>
2024-09-12 15:10:27 +0530

    added the login link at signup page"

--- src/App.js
+++ src/App.js
@@ -2,15 +2,26 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Signup from "./Signup";
 import Login from "./Login";
 import DSASheet from "./DSA/DsaSheet.jsx";
+import { useState } from "react";
 
 function App() {
+	const [loggedIn, setLoggedIn] = useState(false);
 	return (
 		<div className="min-h-screen flex items-center justify-center bg-gray-100">
 			<Router>
 				<Routes>
 					<Route path="/" element={<Signup />} />
 					<Route path="/login" element={<Login />} />
-					<Route path="/home" element={<DSASheet />} />
+					<Route
+						path="/home"
+						element={
+							loggedIn ? (
+								<DSASheet />
+							) : (
+								<Login setLoggedIn={setLoggedIn} />
+							)
+						}
+					/>
 				</Routes>
 			</Router>
 		</div>
